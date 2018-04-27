40ebb4f409a2192ac19c6a3ac3912f0ceec460f6
Darkprodigy001 <36748853+Darkprodigy001@users.noreply.github.com>
2018-04-27 11:50:28 +1000

    Initial commit of working project

---
+++ Assets/Will/FadeIn.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn: MonoBehaviour
+{
+
+    Renderer r;
+
+    // Use this for initialization
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        FromBlack();
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    if (Input.GetKeyDown(KeyCode.Q))
+    //    {
+    //        StartCoroutine("ToBlack");
+
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.W))
+    //    {
+    //        StartCoroutine("FromBlack");
+
+    //    }
+
+    //}
+
+    public void FromBlack()
+    {
+        StartCoroutine("FadeFromBlack");
+    }
+
+    IEnumerator FadeFromBlack()
+        {
+            for (float f = 1f; f >= 0; f -= 0.01f)
+            {
+                Color c = r.material.color;
+                c.a = f;
+                r.material.color = c;
+                yield return null;
+            }
+        }
+
+
+
+        public void ToBlack()
+        {
+            StartCoroutine("FadeInScreen");
+        }
+
+
+
+        IEnumerator FadeInScreen()
+    {
+        for (float f = 0f; f <= 1; f += 0.01f)
+        {
+            Color c = r.material.color;
+            c.a = f;
+            r.material.color = c;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Will/SceneTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour
+{
+
+    public FadeIn f2b;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "TrainFront")
+        {
+            Debug.Log("Calling fade to black");
+            f2b.ToBlack();
+
+        }
+    }
+}
