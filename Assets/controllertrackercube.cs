7dcb787f36cf8e7e3d3842495db410d573a1166a
Darkprodigy001 <36748853+Darkprodigy001@users.noreply.github.com>
2018-08-02 15:06:20 +1000

    Work

---
+++ Assets/controllertrackercube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllertrackercube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Renderer R;
+            R = GetComponent<Renderer>();
+
+            R.material.color = Color.red;
+
+        }
+		
+	}
+}
